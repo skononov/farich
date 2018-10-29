6781b9646670a0420806d25dd0da2424bcb9ae5e
Sergey Kononov <S.A.Kononov@inp.nsk.su>
2018-10-29 09:26:12 +0700

    Add optimization option to run.sh. Add default output of run.sh as run.dat.

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ echo '' > run.dat
 
 for ((nl=$nlmin; nl<=nlmax; nl++)); do
     echo -e "\nЧисло слоев $nl"
-    ./farichres -q -N $nl -e $eff -p $pixel -n 1.05 -T 40 $qefile | tee -a run.log | fgrep 'Ошибка угла на трек'
+    ./farichres -q -N $nl -e $eff -p $pixel -n 1.05 -T 40 -m $qefile | tee -a run.log | fgrep 'Ошибка угла на трек'
     err=$(tail -3 run.log | sed -nr 's/Ошибка угла на трек.*: +([0-9.]+)\b.*$/\1/p')
     echo "$nl    $err" >> run.dat
 done
